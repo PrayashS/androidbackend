9ee06072da8894e377634ebfdaa66dba2001767a
Prayash Shrestha <sthaprayash09@gmail.com>
2021-04-19 19:36:31 +0545

    Product Model

---
+++ models/productModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product', {
+    pname: { type: String },
+    pprice: { type: Number },
+    pdesc: { type: String },
+    pimage: { type: String },
+    prating: { type: String }
+})
+
+module.exports = Product;
